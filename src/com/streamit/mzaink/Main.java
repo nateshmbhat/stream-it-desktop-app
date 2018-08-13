17f68690835627f78a47621d4c78d04e8f3f6905
nateshmbhat <nateshmbhat1@gmail.com>
2018-08-13 14:24:29 +0530

    now song object is populated when playing starts and music view improved

--- src/com/streamit/mzaink/Main.java
+++ src/com/streamit/mzaink/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
       @Override
       public void start(Stage primaryStage) throws Exception {
             Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-            primaryStage.setTitle("Hello World");
+            primaryStage.setTitle("Stream It ©");
 
             //TODO: Try keeping the main window smaller in size.
             primaryStage.setMaximized(true);
