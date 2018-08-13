d06c69ddf715f215452c3b70f4d65374b9512362
nateshmbhat <nateshmbhat1@gmail.com>
2018-08-13 14:35:59 +0530

    now song object is populated when playing starts and music view improved

--- src/com/streamit/nateshmbhat/streamIt/server/MyHttpServer.java
+++ src/com/streamit/nateshmbhat/streamIt/server/MyHttpServer.java
@@ -9,117 +9,153 @@ import com.sun.net.httpserver.HttpServer;
 import javax.rmi.CORBA.Util;
 import javax.swing.*;
 import java.io.*;
-import java.net.InetSocketAddress;
+import java.net.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class MyHttpServer {
-      private static int myPort = 8090;
-      private static String myIp;
-      private List<File> mp3Files = new ArrayList<>();
+    private static int myPort = 8090;
+    private static String myIp;
 
-      public static void startServer() {
-            print(File.separator);
-            HttpServer server = null;
+    public static int getMyPort() {
+        return myPort;
+    }
 
-            try {
-                  server = HttpServer.create(new InetSocketAddress(myPort), 0);
-            } catch (IOException e) {
-                  e.printStackTrace();
+    public static String getMyIp() {
+        return myIp;
+    }
+
+    public String getLocalIp() {
+        String ip = "";
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ip = socket.getLocalAddress().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
+
+    public static void startServer() {
+        print(File.separator);
+        HttpServer server = null;
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(myPort), 0);
+            myIp = getMyIp() ;
+            print("Server started at : " + myIp + " , port = " + myPort ) ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.createContext("/getFile", new GetFile());
+        server.createContext("/ping", new PingClass());
+        server.createContext("/getSongs", new GetSongs());
+        server.createContext("/getSongInfo", new GetSongInfo());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+
+    static class PingClass implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+
+            String response = myIp + ":" + myPort;
+            httpExchange.sendResponseHeaders(200, response.length()) ;
+
+            PrintWriter printWriter = new PrintWriter(httpExchange.getResponseBody()) ;
+            printWriter.write(response);
+            printWriter.close();
+        }
+    }
+
+
+    static class GetSongInfo implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+
+            print("handling GetSongInfo");
+
+            OutputStream os = httpExchange.getResponseBody();
+
+            String queryParams = httpExchange.getRequestURI().getQuery();
+
+            print("Query params = " + queryParams);
+
+            String songMetadata = Utility.getAudioMetaData(queryParams);
+
+            PrintWriter printWriter = new PrintWriter(os);
+
+            httpExchange.sendResponseHeaders(200, songMetadata.length());
+            printWriter.write(songMetadata);
+            printWriter.close();
+        }
+    }
+
+
+    static class GetSongs implements HttpHandler {
+        private List<File> mp3Files;
+
+        @Override
+        public void handle(HttpExchange reqres) throws IOException {
+
+            //Done.
+            if (Controller.getFilesFromFX() != null) {
+                mp3Files = Controller.getFilesFromFX();
             }
 
-            Utility.getAudioMetaData("C:\\Users\\Natesh\\Desktop\\ae watan testing .mp3") ;
+            System.out.println("Running getSongs handler .. ");
 
-            server.createContext("/getFile", new GetFile());
-            server.createContext("/getSongs", new GetSongs());
-            server.createContext("/getSongInfo", new GetSongInfo());
-            server.setExecutor(null); // creates a default executor
-            server.start();
-      }
+            HashMap<String, String> postBody = Utility.parsePostRequest(Utility.getPostRequestBody(reqres.getRequestBody()));
+            print(postBody.toString());
 
-      static class GetSongInfo implements HttpHandler{
-            @Override
-            public void handle(HttpExchange httpExchange) throws IOException {
+            OutputStream response = reqres.getResponseBody();
+            StringBuilder paths = new StringBuilder();
 
-                  print("handling GetSongInfo") ;
-
-                  OutputStream os = httpExchange.getResponseBody();
-
-                  String queryParams = httpExchange.getRequestURI().getQuery();
-
-                  print("Query params = " + queryParams);
-
-                  String songMetadata = Utility.getAudioMetaData(queryParams) ;
-
-                  PrintWriter printWriter = new PrintWriter(os) ;
-
-                  httpExchange.sendResponseHeaders(200 , songMetadata.length());
-                  printWriter.write(songMetadata) ;
-                  printWriter.close();
+            for (File songPath : mp3Files) {
+                paths.append(songPath.toPath().toString() + "\n");
             }
-      }
+
+            System.out.println("\n paths String = " + paths);
+            PrintWriter writer = new PrintWriter(response);
+
+            reqres.sendResponseHeaders(200, paths.length());
+            writer.write(paths.toString());
+            writer.close();
+        }
+    }
 
 
-      static class GetSongs implements HttpHandler {
-            private List<File> mp3Files;
+    static class GetFile implements HttpHandler {
 
-            @Override
-            public void handle(HttpExchange reqres) throws IOException {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            //Get requested file which was requested using the GET request
+            OutputStream os = t.getResponseBody();
 
-                  //Done.
-                  if (Controller.getFilesFromFX() != null) {
-                        mp3Files = Controller.getFilesFromFX();
-                  }
+            String queryParams = t.getRequestURI().getQuery();
 
-                  System.out.println("Running getSongs handler .. ");
+            print("Query params = " + queryParams);
 
-                  HashMap<String, String> postBody = Utility.parsePostRequest(Utility.getPostRequestBody(reqres.getRequestBody()));
-                  print(postBody.toString());
+            File file = new File(queryParams);
+            print(file.toPath().toString());
 
-                  OutputStream response = reqres.getResponseBody();
-                  StringBuilder paths = new StringBuilder();
+            t.sendResponseHeaders(200, file.length());
 
-                  for (File songPath : mp3Files) {
-                        paths.append(songPath.toPath().toString() + "\n");
-                  }
+            Files.copy(file.toPath(), os);
+            os.close();
+        }
+    }
 
-                  System.out.println("\n paths String = " + paths);
-                  PrintWriter writer = new PrintWriter(response);
-
-                  reqres.sendResponseHeaders(200, paths.length());
-                  writer.write(paths.toString());
-                  writer.close();
-            }
-      }
-
-
-      static class GetFile implements HttpHandler {
-
-            @Override
-            public void handle(HttpExchange t) throws IOException {
-                  //Get requested file which was requested using the GET request
-                  OutputStream os = t.getResponseBody();
-
-                  String queryParams = t.getRequestURI().getQuery();
-
-                  print("Query params = " + queryParams);
-
-
-                  File file = new File(queryParams);
-                  print(file.toPath().toString());
-
-                  t.sendResponseHeaders(200, file.length());
-
-                  Files.copy(file.toPath(), os);
-                  os.close();
-            }
-      }
-
-      public static void print(String s) {
-            System.out.println(s);
-      }
+    public static void print(String s) {
+        System.out.println(s);
+    }
 
 }
 
